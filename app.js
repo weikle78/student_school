56278d9b4adc91c65a70ad724832b7a78fa6fa7a
Bryan Weikle <weikle78@ksu.edu>
2018-11-01 09:27:38 -0500

    11-1-18 Finished deployment

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const config = {
 	views: 'views', 		// Set views directory 
 	static: 'public', 		// Set static assets directory
 	db: { 					// Database configuration. Remember to set env variables in .env file: MONGODB_URI, PROD_MONGODB_URI
-		//url: (process.env.TURBO_ENV == 'dev') ? process.env.MONGODB_URI : process.env.PROD_MONGODB_URI,
-		url: 'mongodb://student:student1@ds143953.mlab.com:43953/student_app',
+		url: (process.env.TURBO_ENV == 'dev') ? process.env.MONGODB_URI : process.env.PROD_MONGODB_URI,
+
 		type: 'mongo',
 		onError: (err) => {
 			console.log('DB Connection Failed!')
